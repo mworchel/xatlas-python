2cbf92639920a40e3a442b6a6700496604f7ef6e
Markus Worchel <mworchel@hotmail.de>
2021-02-18 21:49:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ class CMakeBuild(build_ext):
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="xatlas",
-    version="0.0.2",
+    version="0.0.3",
     description="Python bindings for xatlas",
     author="Markus Worchel",
     author_email="m.worchel@campus.tu-berlin.de",
